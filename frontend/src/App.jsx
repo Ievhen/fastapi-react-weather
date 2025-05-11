81f6de69d67c8d81cd9e2fbf1d139dab3786e0ea
Ievhen <grabowski.ievhen@gmail.com>
2025-05-11 17:33:33 +0200

    feat: add Sidebar and WeatherCard components to App, fetching data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,36 @@
+import axios from "axios";
+import WeatherCard from "./components/WeatherCard"
+import Sidebar from "./components/Sidebar"
+import { useEffect, useState } from "react";
+
 function App() {
+  const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  const fetchCountries = () => {
+    axios.get('http://127.0.0.1:8000/eu/countries').then(r => {
+      const countries = r.data
+      setCountries(countries)
+    })
+  }
+
+  const changeCountry = (country) => {
+    setSelectedCountry(country)
+  }
+
+  useEffect(() => {
+    fetchCountries()
+  }, []);
+
   return (
     <div className="d-flex flex-grow-1">
+      <Sidebar
+        title='EU Countries'
+        items={countries}
+        onItemClick={(e) => changeCountry(e)}
+      />
       <div className="content d-flex flex-grow-1 justify-content-center align-items-center">
-        Hello world!
+        <WeatherCard country={selectedCountry} />
       </div>
     </div>
   )
