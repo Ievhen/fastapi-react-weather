12d364f25b86892a8e6ad923aec06f566c93ecdf
Ievhen <grabowski.ievhen@gmail.com>
2025-06-08 14:31:37 +0200

    feat: add weather-icons to project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './scss/style.scss'
 import App from './App.jsx'
 
-// Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'weather-icons/css/weather-icons.min.css'
 
 createRoot(document.getElementById('root')).render(
   <App />
